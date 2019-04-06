51137289db7dfc2a6f23bcb2deacfbdbad6e894e
Thijs Alders <thijs@thijsalders.nl>
2019-04-06 20:21:22 +0200

    Remove pusher provider.

--- core/src/main/java/net/craftingstore/core/models/api/provider/ProviderType.java
+++ core/src/main/java/net/craftingstore/core/models/api/provider/ProviderType.java
@@ -1,11 +1,10 @@
 package net.craftingstore.core.models.api.provider;
 
 import net.craftingstore.core.provider.CraftingStoreProvider;
-import net.craftingstore.core.provider.PusherProvider;
 import net.craftingstore.core.provider.SocketProvider;
 
 public enum ProviderType {
-    SOCKET(SocketProviderInformation.class, SocketProvider.class), PUSHER(PusherProviderInformation.class, PusherProvider.class);
+    SOCKET(SocketProviderInformation.class, SocketProvider.class);
 
     private Class c;
     private Class<? extends CraftingStoreProvider> implementation;
