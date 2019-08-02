0417eee0701b62d958e20f8edbeca580b84ccab7
Thijs Alders <thijs@thijsalders.nl>
2019-08-02 23:53:02 +0200

    Update to use custom version of socket.io with bugfixes.

--- sponge/src/main/java/net/craftingstore/sponge/CraftingStoreSponge.java
+++ sponge/src/main/java/net/craftingstore/sponge/CraftingStoreSponge.java
@@ -24,7 +24,7 @@ import org.spongepowered.api.text.format.TextColors;
 
 import java.nio.file.Path;
 
-@Plugin(id = "craftingstore", name = "CraftingStore", version = "2.2.0")
+@Plugin(id = "craftingstore", name = "CraftingStore", version = "2.2.1")
 public class CraftingStoreSponge {
     private CraftingStore craftingStore;
 
