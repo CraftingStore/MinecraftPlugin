51d382a7378b1c95fc89de56728c6c8e9d7c0395
Thijs Alders <t.alders@wearetriple.com>
2021-01-12 21:26:48 +0100

    Fix build of velocity plugin.

--- core/src/main/java/net/craftingstore/core/exceptions/CraftingStoreApiException.java
+++ core/src/main/java/net/craftingstore/core/exceptions/CraftingStoreApiException.java
@@ -5,7 +5,11 @@ import java.io.IOException;
 public class CraftingStoreApiException extends Exception {
 
     public CraftingStoreApiException(String s, IOException e) {
-        super(s, e.getCause());
-        e.printStackTrace();
+        super(s, e);
+    }
+
+    @Override
+    public void printStackTrace() {
+        super.getCause().printStackTrace();
     }
 }
