5baa2fa4ea0db5608a119d3d5882e8120ae143d8
ItsJoonas <fackelmeyer@web.de>
2022-12-14 22:51:52 +0100

    Added missing data from other versions files

--- velocity/src/main/java/net/craftingstore/velocity/events/DonationReceivedEvent.java
+++ velocity/src/main/java/net/craftingstore/velocity/events/DonationReceivedEvent.java
@@ -3,12 +3,75 @@ package net.craftingstore.velocity.events;
 import com.velocitypowered.api.event.ResultedEvent;
 import net.craftingstore.core.models.donation.Donation;
 
+import java.util.UUID;
+
 public class DonationReceivedEvent implements ResultedEvent<DonationResult> {
+    @Deprecated
+    private final String command;
+    @Deprecated
+    private final String username;
+    @Deprecated
+    private final UUID uuid;
+    @Deprecated
+    private final String packageName;
+    @Deprecated
+    private final int packagePrice;
+    @Deprecated
+    private final int couponDiscount;
+
     private Donation donation;
+
+    private boolean cancelled = false;
     private DonationResult result;
 
+    @Deprecated
+    public DonationReceivedEvent(String command, String username, UUID uuid, String packageName, int packagePrice, int couponDiscount) {
+        this.command = command;
+        this.username = username;
+        this.uuid = uuid;
+        this.packageName = packageName;
+        this.packagePrice = packagePrice;
+        this.couponDiscount = couponDiscount;
+    }
+
     public DonationReceivedEvent(Donation donation) {
+        command = donation.getCommand();
+        username = donation.getPlayer().getUsername();
+        uuid = donation.getPlayer().getUUID();
+        packageName = donation.getPackage().getName();
+        packagePrice = donation.getPackage().getPrice();
+        couponDiscount = donation.getDiscount();
         this.donation = donation;
+    }
+
+    @Deprecated
+    public String getCommand() {
+        return command;
+    }
+
+    @Deprecated
+    public String getUsername() {
+        return username;
+    }
+
+    @Deprecated
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    @Deprecated
+    public String getPackageName() {
+        return packageName;
+    }
+
+    @Deprecated
+    public int getPackagePrice() {
+        return packagePrice;
+    }
+
+    @Deprecated
+    public int getCouponDiscount() {
+        return couponDiscount;
     }
 
     public Donation getDonation() {
@@ -22,6 +85,6 @@ public class DonationReceivedEvent implements ResultedEvent<DonationResult> {
 
     @Override
     public void setResult(DonationResult result) {
-        result = result;
+        this.result = result;
     }
 }
